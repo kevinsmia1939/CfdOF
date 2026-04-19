70d217ca94d1085c3c4ed0dd0ce0a6cccc42aa87
Kavin Teenakul <andythe_great@protonmail.com>
2026-04-19 04:25:04 +0200

    fix TemplateBuilder: open template files with utf-8 encoding

--- CfdOF/TemplateBuilder.py
+++ CfdOF/TemplateBuilder.py
@@ -83,7 +83,7 @@ class TemplateBuilder(object):
     def buildFile(self, rel_file, params):
         """ Open the specified template file, make replacements, and return as a string """
         try:
-            fid = open(os.path.join(self.template_path, rel_file))
+            fid = open(os.path.join(self.template_path, rel_file), encoding='utf-8')
         except IOError:
             # Special cases:
             # 1. Don't worry if files that end with "None" do not exist
@@ -92,7 +92,7 @@ class TemplateBuilder(object):
             # 2. If a file is not found, try the same file with 'default' after the last underscore
             rel_file_default = rel_file.rsplit("_", 1)[0] + "_default"
             try:
-                fid = open(os.path.join(self.template_path, rel_file_default))
+                fid = open(os.path.join(self.template_path, rel_file_default), encoding='utf-8')
             except IOError:
                 raise IOError("Error reading file {} in template path {}".format(rel_file, self.template_path))
             finally:
