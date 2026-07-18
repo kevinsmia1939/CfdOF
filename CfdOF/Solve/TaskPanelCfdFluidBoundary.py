cf6d7b69d85d6416145bfe1dd79dc79cddf1df64
Kavin Teenakul <andythe_great@protonmail.com>
2026-07-18 12:36:26 +0200

    Remove boundary region coupling controls

--- CfdOF/Solve/TaskPanelCfdFluidBoundary.py
+++ CfdOF/Solve/TaskPanelCfdFluidBoundary.py
@@ -32,7 +32,6 @@ from FreeCAD import Units
 if FreeCAD.GuiUp:
     import FreeCADGui
     from PySide import QtGui, QtCore
-    from PySide.QtGui import QFormLayout
 from CfdOF import CfdTools
 from CfdOF.CfdTools import getQuantity, setQuantity, indexOrDefault, storeIfChanged
 from CfdOF import CfdFaceSelectWidget
@@ -159,8 +158,6 @@ class TaskPanelCfdFluidBoundary:
         setQuantity(self.form.input_sepy, Units.Quantity(self.obj.PeriodicSeparationVector.y, Units.Length))
         setQuantity(self.form.input_sepz, Units.Quantity(self.obj.PeriodicSeparationVector.z, Units.Length))
 
-        self.initRegionCouplingControls(boundary_patches)
-
         # Turbulence
         if self.turb_model is not None:
             self.form.comboTurbulenceSpecification.addItems(CfdFluidBoundary.TURBULENT_INLET_SPEC[self.turb_model][0])
@@ -231,18 +228,6 @@ class TaskPanelCfdFluidBoundary:
                                                                     self.obj, True, True, False)
 
         self.updateUI()
-
-    def initRegionCouplingControls(self, boundary_patches):
-        self.regionCouplingFrame = QtGui.QGroupBox("Multi-region coupling")
-        regionLayout = QtGui.QFormLayout(self.regionCouplingFrame)
-
-        self.inputRegionName = QtGui.QLineEdit()
-        self.inputRegionName.setToolTip(
-            "OpenFOAM region containing this boundary. If left empty, CfdOF will try to infer it.")
-        self.inputRegionName.setText(getattr(self.obj, 'RegionName', ''))
-        regionLayout.addRow("Region name:", self.inputRegionName)
-
-        self.form.layout().insertWidget(1, self.regionCouplingFrame)
 
     def updateUI(self):
         # Boundary type and subtype
@@ -494,8 +479,6 @@ class TaskPanelCfdFluidBoundary:
 
         storeIfChanged(self.obj, 'PeriodicPartner', self.form.comboBoxPeriodicPartner.currentText())
         storeIfChanged(self.obj, 'PeriodicMaster', self.form.radioButtonMasterPeriodic.isChecked())
-
-        storeIfChanged(self.obj, 'RegionName', self.inputRegionName.text().strip())
 
         # Turbulence
         if self.turb_model in CfdFluidBoundary.TURBULENT_INLET_SPEC:
